74edd10e2a3e629b484184122df75a051e1f9dfd
Vivek <vivek_mandal@outlook.com>
2026-06-22 20:15:37 +0530

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.py
@@ -0,0 +1,23 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+
+        bucket = {}
+
+        for word in strs:
+            count = [0] * 26
+
+            for char in word:
+                count[ord(char) - ord('a')] += 1
+
+            key = tuple(count) 
+
+            if key not in bucket:
+                bucket[key] = [word]
+            else:
+                bucket[key].append(word)
+
+        print(bucket)
+        return list(bucket.values())     
+
+
+        
